8c72d2b6151cafd7462c3783b165a70765d6c484
TaixMiguel <48259444+TaixMiguel@users.noreply.github.com>
2023-06-08 20:26:28 +0200

    [CHANGED] Cambio de visibilidad sobre métodos de `ConfigApp` (#5)
    Se definen todos los métodos de acceso a `Preferences` como protected

--- lib/FoodDispenser/include/ConfigApp.h
+++ lib/FoodDispenser/include/ConfigApp.h
@@ -11,13 +11,13 @@ class ConfigApp {
   public:
     ConfigApp(const char* appName="autdisp");
 
+  protected:
     virtual bool getBool(String key, bool valueDefault=false);
     virtual float getFloat(String key, float valueDefault=-1);
     virtual int getInt(String key, int valueDefault=-1);
     virtual long getLong(String key, long valueDefault=-1);
     virtual String getString(String key, String valueDefault="");
 
-  protected:
     virtual void saveBool(String key, bool value);
     virtual void saveFloat(String key, float value);
     virtual void saveInt(String key, int value);
